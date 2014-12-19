cc394f22d7fe28c2fe26faabc2e5dcefd93c6080
Brian Woodward <brian.woodward@gmail.com>
2014-12-19 12:05:11 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * progressive <https://github.com/doowb/progressive>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
